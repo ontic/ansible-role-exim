28ccfccdc31faf1f122a030d512f1d3d7a276691
Adam Dyson <adam.dyson@outlook.com>
2018-05-22 22:53:32 +1000

    Added Ansible 2.4 support

--- tests/test-verify.sh
+++ tests/test-verify.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Copyright (c) Ontic. (http://www.ontic.com.au). All rights reserved.
+# See the COPYING file bundled with this package for license details.
 
 # Verify the mail name has be updated.
 docker exec --tty ${container_id} env TERM=xterm cat /etc/mailname
